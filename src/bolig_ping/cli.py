29e75b59ec5c7f873929fe2dd3ef7fdfe77b19cd
Dan Saattrup Nielsen <saattrupdan@gmail.com>
2025-03-24 14:46:57 +0100

    feat: Add --update-interval

--- src/bolig_ping/cli.py
+++ src/bolig_ping/cli.py
@@ -1,6 +1,7 @@
 """Command line interface for the project."""
 
 import logging
+from time import sleep
 
 import click
 
@@ -50,6 +51,12 @@ logger = logging.getLogger("jinn")
     help="The minimum size of the apartment, in square meters.",
 )
 @click.option("--query", "-q", multiple=True, help="A query to filter the results by.")
+@click.option(
+    "--update-interval-hours",
+    type=int,
+    default=None,
+    help="The interval in hours to update the search results. If None, only run once.",
+)
 def main(
     city: list[str],
     email: str,
@@ -57,6 +64,7 @@ def main(
     min_rooms: int,
     min_size: int,
     query: list[str],
+    update_interval_hours: int | None,
 ) -> None:
     """Search for flats in Denmark.
 
@@ -73,6 +81,8 @@ def main(
             A query to filter the results by.
         email:
             Email address to send the notification to, or None to print to stdout.
+        update_interval_hours:
+            The interval in hours to update the search results. If None, only run once.
     """
     cities = [
         c.replace(" ", "-")
@@ -89,10 +99,27 @@ def main(
         min_size=min_size,
         queries=query,
     )
+    if update_interval_hours is None:
+        find_new_flats(search_query=search_query, email=email)
+    else:
+        while True:
+            find_new_flats(search_query=search_query, email=email)
+            logger.info(f"Sleeping for {update_interval_hours:,} hours...")
+            sleep(update_interval_hours * 3600)
+
+
+def find_new_flats(search_query: SearchQuery, email: str) -> None:
+    """Find new flats that satisfy the search query and send them to the email.
+
+    Args:
+        search_query:
+            The search query to find flats for.
+        email:
+            The email to send the flats to.
+    """
     flats = scrape_results(search_query=search_query)
     flats = remove_cached_flats(flats=flats, email=email)
     logger.info(f"Found {len(flats)} new flats that satisfy the search query.")
-
     if flats:
         send_flats(to_email=email, flats=flats)
         store_to_cache(flats=flats, email=email)
